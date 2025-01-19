262278e4f6e8bb87dc94d7297030c6b0b835a74e
Henrique Dergado <47786577+henriquedergado@users.noreply.github.com>
2025-01-19 13:22:39 -0300

    Update appYoutubeTranscript.py

--- appYoutubeTranscript.py
+++ appYoutubeTranscript.py
@@ -2,6 +2,14 @@
 import streamlit as st # Importando a biblioteca Streamlit para criação de aplicações web
 from langchain_community.document_loaders import YoutubeLoader
 
+from langchain.llms import OpenAI # Importando o modelo de linguagem da OpenAI
+from langchain.prompts import PromptTemplate # Importando a classe para templates de prompt
+from langchain.chains import LLMChain, SequentialChain # Importando classes para criar cadeias de LLM
+from langchain_openai import ChatOpenAI
+
+# Configurando a chave de API da OpenAI no ambiente
+os.environ['OPENAI_API_KEY'] = st.secrets["openai_api_key"]
+
 # Adicionar a imagem no cabeçalho
 image_url = "https://www.seccoattuy.com.br/wp-content/uploads/2022/10/logo.png"
 st.image(image_url, width=300)
@@ -9,6 +17,19 @@ st.image(image_url, width=300)
 # Adicionar o nome do aplicativo
 st.subheader("Youtube transcripts - Secco Attui")
 link = st.text_input('🔗 Digite o link do vídeo...') # Campo de entrada para o usuário escrever o tema
+
+# Definindo templates de prompt para o título do vídeo e o roteiro
+blog_template = PromptTemplate(
+    input_variables = ['transcription'], 
+    template = """
+    Atue como redador especializado em Copywriting, Escreva um post para um blog Wordpress resumindo a transcrição... {transcription}
+    
+    <regras>
+     - O texto deve ter pelo menos 3000 caracteres
+     - Termine o texto sempre provocando o leitor a interagir nos comentários com base em sua opinião.
+    <regras>
+    """
+)
 
 # Mostrando os resultados na tela se houver um prompt
 if link:
@@ -22,6 +43,15 @@ if link:
     # Verifica se o primeiro item é um objeto do tipo Document
     if hasattr(result[0], 'page_content'):
         page_content = result[0].page_content  # Acessa o atributo diretamente
-        st.write(page_content)  # Exibe o conteúdo no Streamlit
+        with st.expander('Transcrição'): 
+            st.info(page_content)
+
+        llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.8)
+        # Configurando a cadeia de LLM para gerar títulos
+        blog_chain = LLMChain(llm=llm, prompt=blog_template, verbose=True, output_key='post')
+        post = blog_chain.run(transcription=page_content)
+
+        with st.expander('post para Blog'): 
+            st.info(post)
     else:
         st.write("O atributo 'page_content' não está disponível no objeto.")
