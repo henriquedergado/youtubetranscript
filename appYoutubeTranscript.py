4414b71121df469d4fc7b610cf205105d47a4c9a
Henrique Dergado <47786577+henriquedergado@users.noreply.github.com>
2025-01-20 11:38:33 -0300

    Update appYoutubeTranscript.py

--- appYoutubeTranscript.py
+++ appYoutubeTranscript.py
@@ -11,22 +11,29 @@ from langchain_openai import ChatOpenAI
 # Configurando a chave de API da OpenAI no ambiente
 os.environ['OPENAI_API_KEY'] = st.secrets["openai_api_key"]
 
+headers = {
+    'Authorization': 'Bearer ' + st.secrets["jina_api_key"] 
+}
+
 # Adicionar a imagem no cabeçalho
 image_url = "https://www.seccoattuy.com.br/wp-content/uploads/2022/10/logo.png"
 st.image(image_url, width=300)
 
 # Adicionar o nome do aplicativo
-st.subheader("Youtube transcripts - Secco Attui")
-link = st.text_input('🔗 Digite o link do vídeo...') # Campo de entrada para o usuário escrever o tema
+st.subheader("Youtube transcripts - Wordpress Blog generator")
+link = st.text_input('🔗 Digite o link do Youtube para transcrição...') # Campo de entrada para o usuário escrever o tema
+link_ref_artigo = st.text_input('🔗 Digite o link de um post para referência de tom de voz...')
 
 # Definindo templates de prompt para o título do vídeo e o roteiro
 blog_template = PromptTemplate(
-    input_variables = ['transcription'], 
+    input_variables = ['transcription', 'article'], 
     template = """
     Atue como redador especializado em Copywriting, Escreva um post para um blog Wordpress resumindo a transcrição... {transcription}
     
+    Utilize como referência de tom de voz o artigo... {article}
     <regras>
      - O texto deve ter pelo menos 3000 caracteres
+     - Não invente dados, utilize a transcrição como fonte de conhecimento
      - Termine o texto sempre provocando o leitor a interagir nos comentários com base em sua opinião.
     <regras>
     """
@@ -44,13 +51,18 @@ if link:
     # Verifica se o primeiro item é um objeto do tipo Document
     if hasattr(result[0], 'page_content'):
         page_content = result[0].page_content  # Acessa o atributo diretamente
+
+        st.write('Aqui está a transcrição do vídeo')
         with st.expander('Transcrição'): 
             st.info(page_content)
+
+        st.write('Agora estamos gerando o texto do Blog, aguarde...')
 
         llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.8)
         # Configurando a cadeia de LLM para gerar títulos
         blog_chain = LLMChain(llm=llm, prompt=blog_template, verbose=True, output_key='post')
-        post = blog_chain.run(transcription=page_content)
+        article = requests.get('https://r.jina.ai/'+link_ref_artigo, headers=headers)
+        post = blog_chain.run(transcription=page_content, article=article.text)
 
         with st.expander('post para Blog'): 
             st.info(post)
